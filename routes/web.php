5dd69cf96244e2d3a59366bc555bce157a925419
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-07-14 14:22:44 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//simple route
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+//redirect route
+Route::get('/hallo', function () {
+    return redirect()->name('hello');
+});
+
+//dynamic route
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello, ' . $name . '!';
+});
+
+//fallback route
+Route::fallback(function () {
+    return 'still got somewhere';
+});
